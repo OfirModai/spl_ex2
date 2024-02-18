103a2b96b5aaa4c9db95c5e8389865b41ccdf1c3
Ofir Modai <ofir789987@gmail.com>
2024-02-18 16:05:54 +0200

    lala

--- src/main/java/bguspl/set/ex/Dealer.java
+++ src/main/java/bguspl/set/ex/Dealer.java
@@ -1,11 +1,10 @@
 package bguspl.set.ex;
-
 import bguspl.set.Env;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.stream.Collectors;
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
 /**
